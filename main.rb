335a2a9eac385aecb5f90d3d7a932ab007f3dda7
Samuel ODaniels <samodan195@gmail.com>
2022-06-15 17:53:39 +0100

    Add main.rb file with Linked Lists example code

---
+++ main.rb
@@ -0,0 +1,9 @@
+require './linked_lists'
+
+# Linked_list class can be used like so
+ll = LinkedList.new
+ll.append('new head')
+ll.append('new tail')
+puts ll
+
+# more methods can be found in 'linked_lists.rb'
